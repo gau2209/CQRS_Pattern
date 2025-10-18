08b4a68c8e9c5e56b3bf4b9869ab73692d44e9b0
Gau2209 <anhtuanvo2209@gmail.com>
2025-10-18 19:05:20 +0700

    install MediatR

--- CQRS_Pattern/Program.cs
+++ CQRS_Pattern/Program.cs
@@ -15,6 +15,10 @@ namespace CQRS_Pattern
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddDbContext<MyDBContext>(options => options.UseInMemoryDatabase("MyDBContext"));
+            builder.Services.AddMediatR(configuration => 
+            {
+                configuration.RegisterServicesFromAssembly(typeof(Program).Assembly);
+            });
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
 
