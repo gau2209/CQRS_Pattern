02a61bcb208903636fe41d8254c9c8def9737ced
Gau2209 <anhtuanvo2209@gmail.com>
2025-10-18 19:24:57 +0700

    Command MediatR

---
+++ CQRS_Pattern/Features/CreateStudent/CreateStudentCommand.cs
@@ -0,0 +1,7 @@
+﻿using MediatR;
+
+namespace CQRS_Pattern.Features.CreateStudent
+{
+    public record CreateStudentCommand(string Name,int Age) : IRequest<Guid>;
+
+}
---
+++ CQRS_Pattern/Features/CreateStudent/CreateStudentCommandHandler.cs
@@ -0,0 +1,28 @@
+﻿using CQRS_Pattern.AppDBContext;
+using CQRS_Pattern.Entity;
+using MediatR;
+
+namespace CQRS_Pattern.Features.CreateStudent
+{
+    public class CreateStudentCommandHandler : IRequestHandler<CreateStudentCommand, Guid>
+    {
+        private readonly MyDBContext _context;
+
+        public CreateStudentCommandHandler (MyDBContext context)
+        {
+            _context = context;
+        }
+        public async Task<Guid> Handle (CreateStudentCommand request, CancellationToken cancellationToken)
+        {
+            Student stu = new Student( )
+            {
+                ID = Guid.NewGuid( ),
+                Name = request.Name,
+                Age = request.Age
+            };
+            _context.Students.Add(stu);
+            await _context.SaveChangesAsync();
+            return stu.ID;
+        }
+    }
+}
