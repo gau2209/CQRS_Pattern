99bd7ed06608e5fbb042ab07ccaf5289a412c2ee
Gau2209 <anhtuanvo2209@gmail.com>
2025-10-18 19:25:12 +0700

    Query

---
+++ CQRS_Pattern/Features/GetStudentByID/GetStudentByIDCommand.cs
@@ -0,0 +1,8 @@
+﻿using CQRS_Pattern.Entity;
+using MediatR;
+
+namespace CQRS_Pattern.Features.GetStudentByID
+{
+    public record GetStudentByIDCommand(Guid ID) : IRequest<Student>;
+
+}
---
+++ CQRS_Pattern/Features/GetStudentByID/GetStudentByIDCommandHandler.cs
@@ -0,0 +1,20 @@
+﻿using CQRS_Pattern.AppDBContext;
+using CQRS_Pattern.Entity;
+using MediatR;
+
+namespace CQRS_Pattern.Features.GetStudentByID
+{
+    public class GetStudentByIDCommandHandler : IRequestHandler<GetStudentByIDCommand, Student>
+    {
+        private readonly MyDBContext _context;
+
+        public GetStudentByIDCommandHandler (MyDBContext context)
+        {
+            _context = context;
+        }
+        public async Task<Student> Handle (GetStudentByIDCommand request, CancellationToken cancellationToken)
+        {
+            return await _context.Students.FindAsync(request.ID);
+        }
+    }
+}
